6183639ee0bdd8efd797d45f273741dca2425018
christopher gonzalez <christopher.gonzalez98@gmail.com>
2023-01-14 16:07:10 -0600

    changes to html and javaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,6 +35,28 @@ searchBtn.addEventListener('click', ()=> {
 
 function getData() {
     let text = searchField.value;
+    var omdbKey = "40e6cec5"
+
+fetch("https://www.omdbapi.com/?apikey=" + omdbKey + "&t=" + text)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+    console.log(data.Actors);
+  })
+  .catch(function(error) {
+    console.error(error)
+  })
+
 }
 
 createBtn.onclick = createWatchList;
+
+$(function () {
+    $('#movies').sortable({
+      placeholder: 'ui-state-highlight',
+    });
+  });
+
+
